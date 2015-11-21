d5e0e236d2d0486a305b9c230f9f3d88549b9739
sharmainemanuel <manuelsharmaine@gmail.com>
2015-11-21 10:47:24 +0800

    udpated config
    new updates

--- app/app.js
+++ app/app.js
@@ -6,17 +6,18 @@ var app = angular.module("myapp",["ngRoute"]);
 			templateUrl: "views/main.html",
 			controller: "MainController"
 		})
+		.when("/about", {
+			templateUrl: "views/about.html"
+		})
 		.when("/cart", {
 			templateUrl: "views/cart.html"
 		})
-		.when("/groceries", {
+		.when("/grocer" , {
 			templateUrl: "views/grocer.html"
-		})
-		.when("/about", {
-			templateUrl: "views/about.html"
 		})
 		.when("/login", {
 			templateUrl: "views/login.html",
 			controller: "LoginController"
-		});
+		})
+		.otherwise({redirectTo:"/about"});
 	});
--- app/controller/login.js
+++ app/controller/login.js
@@ -1,15 +1,13 @@
 
-app.controller("LoginController",function($scope,$http,$location){
+app.controller("LoginController",function($scope,$http,$window){
 	$scope.submit = function(){
 		var user = $scope.username;
 		var pass = $scope.password;
 		$http.post("http://localhost/smart/login.php",{username:user,password:pass}).then(onUserComplete, onError);
 	};
 	var onUserComplete = function(response){
-		var hasAccount = response.data;
-		if(hasAccount){
-			$location.path("/");
-		}
+		$scope.user = response.data;
+		alert($scope.user);
 	}
 	var onError = function(reason){
 		$scope.error = "Error fetching the data. ";
